6aeb659cfeea86920627121b9b13e23819032807
Nakrit Aree <nakritaree@gmail.com>
2024-03-10 16:10:03 +0700

    len update

--- len.c
+++ len.c
@@ -1,11 +1,17 @@
-#include<stdio.h>
-#include<string.h>
-int main(){
-    char input[100];
-    printf("Insert word : ");
-    scanf("%[^\n]",input);
-    int lenstr = strlen(input);
-    printf("Length of word = %d",lenstr);
+#include <stdio.h>
 
-    
+int main() {
+    char input[100];
+    int chr = 0;
+
+    printf("Insert word: ");
+    scanf("%[^\n]", input);
+
+    for (int i = 0; input[i] != '\0'; i++) {
+        chr = i + 1;  
+    }
+
+    printf("Length of this word = %d", chr);
+
+    return 0;
 }
