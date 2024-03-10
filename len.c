f1a2198555185291ef248e7fe6a407b4c27f7ec4
Nakrit Aree <nakritaree@gmail.com>
2024-03-10 16:32:44 +0700

    len.c

--- len.c
+++ len.c
@@ -1,17 +1,10 @@
-#include <stdio.h>
 
-int main() {
-    char input[100];
-    int chr = 0;
-
-    printf("Insert word: ");
-    scanf("%[^\n]", input);
-
-    for (int i = 0; input[i] != '\0'; i++) {
+int lenchr(char *input) {
+    int chr = 0, i;
+    
+    for (i = 0; input[i] != '\0'; i++) {
         chr = i + 1;  
     }
-
-    printf("Length of this word = %d", chr);
-
-    return 0;
+    
+    return chr;
 }
--- main.c
+++ main.c
@@ -3,19 +3,25 @@
 #include "sub.c"
 #include "multitple.c"
 #include "division.c"
+#include "len.c"
 
 int main()
 {
 	int a, b;
+	char input[100];
 
-	printf("Insert a number\n");
-	scanf("%d", &a);
-	scanf("%d", &b);
 
-	printf("The + of %d and %d is %d\n", a, b, add(a, b));
-	printf("The - of %d and %d is %d\n", a, b, sub(a, b));
-	printf("The * of %d and %d is %d\n", a, b, mul(a, b));
-	printf("The / of %d and %d is %d\n", a, b, div(a, b));
+	// printf("Insert a number : \n");
+	// scanf("%d", &a);
+	// scanf("%d", &b);
+	printf("Insert a word : \n");
+	scanf("%[^\n]",input);
+
+	// printf("The + of %d and %d is %d\n", a, b, add(a, b));
+	// printf("The - of %d and %d is %d\n", a, b, sub(a, b));
+	// printf("The * of %d and %d is %d\n", a, b, mul(a, b));
+	// printf("The / of %d and %d is %d\n", a, b, div(a, b));
+	printf("The length of word = %d",lenchr(input));
 
 	return 0;
 }
